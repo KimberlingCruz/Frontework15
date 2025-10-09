153adf18f6daafc6fa0987202241667fb4b11168
KimberlingCruz <cruzkimberling81@gmail.com>
2025-10-09 11:11:38 -0600

    Se subio archivo Framework

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import{useState} from "react";
+import './App.css';
+import Titulo from "./components/Titulo.jsx";
+import Mensaje from "./components/Mensaje.jsx";
+const App = () => {
+  return ( 
+  <>
+<h1> Vet Killy </h1>
+<Titulo/>
+<Mensaje/> 
+  </>
+  );
+}
+export default App;
---
+++ src/components/Titulo.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Titulo = () => {
+
+  return (
+   <>
+    <h1>Hola Mundo</h1>
+    <h2>Estoy copiando codigos en React con vite</h2>
+     <h3>Ayuda a no estresarse</h3>
+     </>
+
+  );
+
+  }
+
+  export default Titulo;
